6114fec6ab8894b67a2d1257a92a51c3c786fbc8
Vinzenz Aubry <vinzenz@weboffice.de>
2017-05-29 12:37:35 +0100

    added iphonedoubletap script

--- demos/Full Demo/js/hoverontouch.js
+++ demos/Full Demo/js/hoverontouch.js
@@ -1,4 +1,4 @@
-//  jshint ignore: start
+// jshint ignore: start
 
 //  hoverontouch.js
 //  An alternative hover function on mobile devices.
@@ -23,12 +23,12 @@
 
 
 function HoverOnTouch() {
-    //this.loadHammer();
-    //if hammer loaded init, load with js callback
     this.init();
+    this.removeIphoneDT();
 }
 
 HoverOnTouch.prototype.init = function () {
+
     var all_objects = document.getElementsByClassName('testObject');
 
     //Add Hammer Listener to every object
@@ -93,6 +93,31 @@ HoverOnTouch.prototype.init = function () {
     //         // event.preventDefault();
     //     });
     // }
+};
+
+HoverOnTouch.prototype.removeIphoneDT = function () {
+    // remove Iphone Double Tap Effect
+    // by rcmachado - https://gist.github.com/rcmachado/7303143
+    var touch = 'ontouchstart' in document.documentElement
+        || (navigator.MaxTouchPoints > 0)
+        || (navigator.msMaxTouchPoints > 0);
+
+    if (touch) { // remove all :hover stylesheets
+    try { // prevent exception on browsers not supporting DOM styleSheets     properly
+        for (var si in document.styleSheets) {
+            var styleSheet = document.styleSheets[si];
+            if (!styleSheet.rules) continue;
+
+            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                if (!styleSheet.rules[ri].selectorText) continue;
+
+                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                    styleSheet.deleteRule(ri);
+                }
+            }
+        }
+    } catch (ex) {}
+    } 
 };
 
                                         // ===== Device Stuff ==== //
